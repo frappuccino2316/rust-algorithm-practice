bdd68ddaecc41b942b955df640bd8caba70a95c1
frappuccino2316 <frappuccino2316@gmail.com>
2022-04-26 08:32:00 +0900

    O(N^2)

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,22 @@ use proconio::input;
 fn main() {
     input! {
         n: i64,
-        x: i64,
-        y: i64,
+        a: i64
     }
 
     let mut i = 1;
+    let mut j = 1;
+    let mut c = 0;
 
-    while i < n {
-        if i % x == 0 || i % y == 0 {
-            println!("{}", i);
+    while i <= n {
+        while j <= n + 1 {
+            if i + j <= a {
+                c += 1;
+            }
+            j += 1;
         }
         i += 1;
     }
+
+    println!("{}", c);
 }
