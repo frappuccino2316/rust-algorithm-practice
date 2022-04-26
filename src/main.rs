47381f0ca88fd90f0d1c95537ca2c5f2be7ed0ac
frappuccino2316 <frappuccino2316@gmail.com>
2022-04-26 12:47:13 +0900

    逆

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,9 @@
 
 fn main() {
     let v = vec!["A", "B", "C", "D"];
-    // 1 << d := pow(2, d)
     for i in 0..1 << v.len() {
-        println!("===");
-        // -- (1)
         for (j, el) in v.iter().enumerate() {
-            // iのj番目ビットが立っているか -> jがiに含まれるか
-            println!("i: {}", i);
-            println!("j: {}", j);
-            if (1 << j) & i == 0 {
-                // -- (2)
-                // 又は if i >> j & 1 {
+            if (1 << j) & i != 0 {
                 print!("{}", el);
             }
         }
