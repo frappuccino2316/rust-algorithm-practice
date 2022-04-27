3072fe567361dbf0605aaf6b075f8c398fe5b6f6
frappuccino2316 <frappuccino2316@gmail.com>
2022-04-27 23:22:12 +0900

    素数を判定する関数を作成

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,21 @@
-use chrono::{DateTime, Local};
 use proconio::input;
 
 fn main() {
     input! {
-        mut n: i64,
+        n: i64,
     }
 
-    let local_datetime_before: DateTime<Local> = Local::now();
-
-    is_prime(n);
-
-    let local_datetime_after: DateTime<Local> = Local::now();
-
-    let code_duration = local_datetime_after - local_datetime_before;
-    println!("time: {}", code_duration.num_milliseconds());
+    println!("{}", is_prime(n));
 }
 
-fn is_prime(x: i64) {
-    for i in 2..x + 1 {
-        if i == 2 {
-            println!("2");
-            continue;
-        }
+fn is_prime(x: i64) -> bool {
+    let f = x as f64;
+    let limit = f.sqrt() as i64;
 
-        let f = i as f64;
-        let m = f.sqrt() as i64;
-
-        for j in 2..i {
-            if m < 2 {
-                println!("{}", i);
-                break;
-            }
-            if i % j == 0 {
-                break;
-            }
-            if j == m {
-                println!("{}", i);
-            }
+    for i in 2..limit + 1 {
+        if x % i == 0 {
+            return false;
         }
     }
+    true
 }
