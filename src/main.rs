3e0584b07c5e778c10e683ecf5c2695d04acef4c
frappuccino2316 <frappuccino2316@gmail.com>
2022-04-28 00:00:33 +0900

    素因数分解

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,40 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i64,
+        mut n: i64,
     }
 
-    println!("{}", is_prime(n));
+    let f = n as f64;
+    let m = f.sqrt() as i64;
+
+    let mut v = vec![];
+
+    for i in 2..n + 1 {
+        if i > m {
+            break;
+        }
+        if is_prime(i) {
+            v.push(i);
+        }
+    }
+
+    let mut index = 0;
+    let mut result = vec![];
+    while index < v.len() {
+        if n % v[index] == 0 {
+            result.push(v[index]);
+            n /= v[index];
+        } else {
+            index += 1;
+        }
+    }
+    if is_prime(n) && n != 1 {
+        result.push(n);
+    }
+
+    let list: Vec<String> = result.iter().map(|a| a.to_string()).collect();
+
+    println!("{}", list.join(" × "));
 }
 
 fn is_prime(x: i64) -> bool {
