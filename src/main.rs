3f29d406d5e2f2271fc0b565207aa0eb1f208ccc
frappuccino2316 <frappuccino2316@gmail.com>
2022-04-28 23:10:22 +0900

    3以上のNの最大公約数を計算

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
 use proconio::input;
 
+// Nが３個以上の場合の最大公約数を出す
 fn main() {
     input! {
-        mut m: i64,
         mut n: i64,
     }
-    println!("{}", gcd(m, n));
+
+    let mut number_list = Vec::new();
+    let mut result = 0;
+
+    for _ in 0..n {
+        input! {
+            a: i64,
+        }
+        number_list.push(a);
+    }
+
+    for j in 1..n {
+        if j == 1 {
+            result = gcd(number_list[0], number_list[1]);
+        } else {
+            result = gcd(result, number_list[j as usize]);
+        }
+    }
+
+    println!("{}", result);
 }
 
 fn gcd(mut a: i64, mut b: i64) -> i64 {
