87bac9d8734c192120da9fcfea44f88588752e24
frappuccino2316 <frappuccino2316@gmail.com>
2022-04-30 23:20:54 +0900

    組み合わせを計算

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,29 @@ use proconio::input;
 fn main() {
     input! {
         n: i64,
-        r: i64,
     }
 
-    let mut result = 1;
+    let mut one_hundred_count = 0;
+    let mut two_hundred_count = 0;
+    let mut three_hundred_count = 0;
+    let mut four_hundred_count = 0;
 
-    for i in 0..r {
-        result *= n - i;
+    for _ in 0..n {
+        input! {
+            a: i64,
+        }
+
+        match a {
+            100 => one_hundred_count += 1,
+            200 => two_hundred_count += 1,
+            300 => three_hundred_count += 1,
+            400 => four_hundred_count += 1,
+            _ => println!("no matched"),
+        }
     }
 
-    for i in 0..r {
-        result /= r - i;
-    }
-
-    println!("nCr: {}", result);
+    println!(
+        "500 pattern is {}",
+        one_hundred_count * four_hundred_count + two_hundred_count * three_hundred_count
+    );
 }
