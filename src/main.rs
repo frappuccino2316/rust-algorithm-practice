2a7a01b9b868447f435a5f160f7c851b0a9bd7f8
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-01 00:05:26 +0900

    組み合わせを全探索

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Local};
 use proconio::input;
 
 fn main() {
@@ -5,29 +6,37 @@ fn main() {
         n: i64,
     }
 
-    let mut red_count = 0;
-    let mut blue_count = 0;
-    let mut yellow_count = 0;
+    let mut v = Vec::new();
 
     for _ in 0..n {
         input! {
             a: i64,
         }
-
-        match a {
-            1 => red_count += 1,
-            2 => blue_count += 1,
-            3 => yellow_count += 1,
-            _ => println!("no matched"),
-        }
+        v.push(a);
     }
 
-    let result = red_count * (red_count - 1) / 2
-        + blue_count * (blue_count - 1) / 2
-        + yellow_count * (yellow_count - 1) / 2;
+    let local_datetime_before: DateTime<Local> = Local::now();
 
-    println!(
-        "if pick up two card, same color pattern is {} pattern",
-        result
-    );
+    let mut i = 0;
+    let mut count = 0;
+
+    while i < v.len() {
+        let mut j = i + 1;
+
+        while j < v.len() {
+            if v[i] + v[j] == 100000 {
+                count += 1;
+            }
+            j += 1;
+        }
+
+        i += 1;
+    }
+
+    println!("{}", count);
+
+    let local_datetime_after: DateTime<Local> = Local::now();
+
+    let code_duration = local_datetime_after - local_datetime_before;
+    println!("time: {}", code_duration.num_milliseconds());
 }
