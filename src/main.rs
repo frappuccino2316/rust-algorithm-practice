a1d409a571f522e80d092bd033a4139c59721ee6
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-02 21:45:47 +0900

    北市を計算

--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i64,
-    }
-
-    let mut b = Vec::new();
-    let mut r = Vec::new();
-
-    for _ in 0..n {
-        input! {
-            a: i64,
-        }
-        b.push(a);
-    }
-
-    for _ in 0..n {
-        input! {
-            a: i64,
-        }
-        r.push(a);
+        n: f64,
+        a: f64,
+        b: f64
     }
 
     let local_datetime_before: DateTime<Local> = Local::now();
 
-    let n = n as f64;
-    let mut b_total = 0;
-    for i in b {
-        b_total += i;
-    }
-
-    let mut r_total = 0;
-    for i in r {
-        r_total += i;
-    }
-
-    let b_total = b_total as f64;
-    let r_total = r_total as f64;
-
-    println!("{}", (b_total + r_total) / n);
+    println!("{}", ((a / 3.0) + ((b * 2.0) / 3.0)) * n);
 
     let local_datetime_after: DateTime<Local> = Local::now();
 
