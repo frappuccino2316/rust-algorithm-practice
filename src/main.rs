c9bcb545ff1b8a9f86efdd1fae476b9c5846eab8
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-02 23:11:55 +0900

    πを近似で計算

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
-use chrono::{DateTime, Local};
 use proconio::input;
+use rand::Rng;
 
 fn main() {
+    // 試行回数
     input! {
-        n: f64,
-        a: f64,
-        b: f64
+        n: i64,
     }
 
-    let local_datetime_before: DateTime<Local> = Local::now();
+    let n_f64 = n as f64;
+    let mut m = 0.0;
+    let mut rng = rand::thread_rng();
 
-    println!("{}", ((a / 3.0) + ((b * 2.0) / 3.0)) * n);
+    for _ in 0..n {
+        let x = rng.gen_range(0.0..=1.0);
+        let y = rng.gen_range(0.0..=1.0);
 
-    let local_datetime_after: DateTime<Local> = Local::now();
-
-    let code_duration = local_datetime_after - local_datetime_before;
-    println!("time: {}", code_duration.num_milliseconds());
+        if x * x + y * y <= 1.0 {
+            m += 1.0;
+        }
+    }
+    println!("π: {}", 4.0 * m / n_f64);
 }
