388ce05dc919f56fe3b6ab78779da944b6b0f062
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-05 00:26:51 +0900

    フィボナッチ

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,24 @@
 use proconio::input;
-use std::cmp::min;
 
 fn main() {
     input! {
         n: i64,
     }
-
     let n_usize = n as usize;
-    let mut scaffold = vec![0_i64; n_usize + 1];
 
-    for i in scaffold.iter_mut().skip(1).take(n_usize) {
-        input! {
-            h: i64,
-        }
-        *i = h;
-    }
+    let mut v = vec![0; n_usize];
 
-    let mut result = vec![0; n_usize + 1];
-
-    for i in 1..n_usize + 1 {
-        if i == 1 {
-            result[i] = 0;
+    for i in 0..n_usize {
+        if i == 0 {
+            v[i] = 1;
         }
-        if i == 2 {
-            let difference = scaffold[i] - scaffold[i - 1];
-            result[i] = difference.abs();
+        if i >= 1 {
+            v[i] = 2;
         }
-        if i >= 3 {
-            let step_from_one_previous = result[i - 1] + (scaffold[i] - scaffold[i - 1]).abs();
-            let step_from_two_previous = result[i - 2] + (scaffold[i] - scaffold[i - 2]).abs();
-            result[i] = min(step_from_one_previous, step_from_two_previous);
+        if i >= 2 {
+            v[i] = v[i - 1] + v[i - 2];
         }
     }
 
-    println!("min step: {}", result[n_usize]);
+    println!("{}", v[n_usize - 1]);
 }
