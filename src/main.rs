794776ed8c294800452c588df8059aec8eca661b
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-07 18:44:08 +0900

    点Aと線分BCの最短距離を計算

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,27 @@
-// N日間勉強する
-// 勉強するとA1だけ学力 (study_point)が上がる
-// ただし2日連続で勉強はできない
-
 use proconio::input;
 
 fn main() {
     input! {
-        n: i64,
+        ax: f64,
+        ay: f64,
+        bx: f64,
+        by: f64,
+        cx: f64,
+        cy: f64,
     }
 
-    let n_usize = n as usize;
+    let answer: f64;
 
-    let mut study_point = Vec::new();
-    for _ in 0..n {
-        input! {
-            p: i64,
-        }
-        study_point.push(p);
+    if bx * ax + by * ay < 0.0 {
+        answer = ((bx - ax).powf(2.0) + (by - ay).powf(2.0)).sqrt();
+    } else if cx * ax + cy * ay < 0.0 {
+        answer = ((cx - ax).powf(2.0) + (by - ay).powf(2.0)).sqrt();
+    } else {
+        let ab = (bx - ax, by - ay);
+        let ac = (cx - ax, cy - ay);
+        answer =
+            (ab.0 * ac.1 - ab.1 * ac.0).abs() / ((cx - bx).powf(2.0) + (cy - by).powf(2.0)).sqrt();
     }
 
-    let mut max_point = vec![0; study_point.len()];
-
-    for i in 0..n_usize {
-        if i == 0 {
-            max_point[i] = study_point[i];
-        } else if i == 1 {
-            if max_point[i - 1] >= study_point[i] {
-                max_point[i] = max_point[i - 1];
-            } else {
-                max_point[i] = study_point[i];
-            }
-        } else if max_point[i - 1] >= study_point[i] + max_point[i - 2] {
-            max_point[i] = max_point[i - 1];
-        } else {
-            max_point[i] = study_point[i] + max_point[i - 2];
-        }
-    }
-
-    println!("max is {}.", max_point[n_usize - 1]);
+    println!("answer is {}", answer);
 }
