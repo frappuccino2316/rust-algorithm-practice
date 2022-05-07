b2638c30a364ce3dd0345ac71da82da75d4b7163
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-07 17:31:26 +0900

    部分和問題を解く

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,45 @@
 use proconio::input;
-use std::cmp::max;
 
 fn main() {
     input! {
         n: i64,
-        max_weight: i64,
+        s: i64,
     }
 
     let n_usize = n as usize;
-    let max_weight_usize = max_weight as usize;
+    let s_usize = s as usize;
 
-    let mut weight_list = Vec::new();
-    let mut value_list = Vec::new();
+    let mut card_list = Vec::new();
 
     for i in 0..n + 1 {
         if i == 0 {
-            weight_list.push(0);
-            value_list.push(0);
+            card_list.push(0);
         } else {
             input! {
-                w: i64,
-                v: i64,
+                a: i64,
             }
-            weight_list.push(w);
-            value_list.push(v);
+            card_list.push(a);
         }
     }
 
     let mut dp = Vec::new();
     for _ in 0..n + 1 {
-        dp.push(vec![-(2_i64.pow(30)); max_weight_usize + 1]);
+        dp.push(vec![false; s_usize + 1]);
     }
 
-    dp[0][0] = 0;
+    dp[0][0] = true;
 
     for i in 1..n_usize + 1 {
-        for j in 0..max_weight_usize + 1 {
-            if (j as i64) < weight_list[i] {
+        for j in 0..s_usize + 1 {
+            if (j as i64) < card_list[i] {
                 dp[i][j] = dp[i - 1][j];
-            } else if (j as i64) >= weight_list[i] {
-                dp[i][j] = max(
-                    dp[i - 1][j],
-                    dp[i - 1][j - (weight_list[i] as usize)] + value_list[i],
-                );
+            } else if (j as i64) >= card_list[i] {
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - (card_list[i] as usize)];
             }
         }
     }
 
-    let mut answer = 0_i64;
     println!("{:?}", dp);
-    for k in 0..max_weight_usize + 1 {
-        answer = max(answer, dp[n_usize][k]);
-    }
-    println!("Answer is {}.", answer);
+    let result = dp[n_usize][s_usize];
+    println!("card can add s?: {}.", result);
 }
