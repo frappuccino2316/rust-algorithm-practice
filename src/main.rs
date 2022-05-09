802dd14d3221589d1e17e1ed25f50138309d1a32
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-10 00:35:11 +0900

    事前に累積和を出しておく

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,8 @@ fn main() {
 
     let local_datetime_before: DateTime<Local> = Local::now();
 
+    let total_sum = total_sum(a);
+
     for i in 0..b.len() {
         if i != 0 {
             let start = if b[i - 1] < b[i] {
@@ -40,7 +42,7 @@ fn main() {
                 b[i - 1] - 1
             };
 
-            total += sum(a[start..end].to_vec());
+            total += total_sum[end] - total_sum[start];
         }
     }
 
@@ -52,12 +54,14 @@ fn main() {
     println!("time: {}", code_duration.num_milliseconds());
 }
 
-fn sum(a: Vec<i64>) -> i64 {
-    if a.len() == 1 {
-        a[0]
-    } else {
-        let r = sum(a[0..a.len() - 1].to_vec());
-
-        a[a.len() - 1] + r
+fn total_sum(a: Vec<i64>) -> Vec<i64> {
+    let mut t = Vec::new();
+    for i in 0..a.len() {
+        if i == 0 {
+            t.push(a[i]);
+        } else {
+            t.push(a[i - 1] + a[i]);
+        }
     }
+    t
 }
