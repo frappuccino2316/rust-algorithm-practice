75557e063edaefd5e8d13447eea911170081b52d
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-10 00:13:02 +0900

    階差から累積和を計算

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,54 @@ use proconio::input;
 
 fn main() {
     input! {
-        x1: i64,
-        y1: i64,
-        x2: i64,
-        y2: i64,
-        x3: i64,
-        y3: i64,
-        x4: i64,
-        y4: i64,
+        n: i64,
+        m: i64,
     }
 
-    let c = judge(x1, y1, x2, y2, x3, y3);
-    let d = judge(x1, y1, x2, y2, x4, y4);
+    let mut a: Vec<i64> = Vec::new();
+    let mut b: Vec<usize> = Vec::new();
+    let mut total: i64 = 0;
 
-    if c * d <= 0 {
-        println!("交差しています");
-    } else {
-        println!("交差していません")
+    for _ in 0..(n - 1) as usize {
+        input! {
+            ai: i64,
+        }
+        a.push(ai);
     }
+
+    for _ in 0..(m) as usize {
+        input! {
+            bi: usize,
+        }
+        b.push(bi);
+    }
+
+    for i in 0..b.len() {
+        if i != 0 {
+            let start = if b[i - 1] < b[i] {
+                b[i - 1] - 1
+            } else {
+                b[i] - 1
+            };
+            let end = if b[i - 1] < b[i] {
+                b[i] - 1
+            } else {
+                b[i - 1] - 1
+            };
+
+            total += sum(a[start..end].to_vec());
+        }
+    }
+
+    println!("total move distance is {}km.", total);
 }
 
-fn judge(x1: i64, y1: i64, x2: i64, y2: i64, x: i64, y: i64) -> i64 {
-    // y-y1=((y1-y2)/(x1-x2))(x-x1) -> (y-y1) * (x1-x2) - (y1-y2) * (x-x1)の正負をチェックする
-    (y - y1) * (x1 - x2) - (y1 - y2) * (x - x1)
+fn sum(a: Vec<i64>) -> i64 {
+    if a.len() == 1 {
+        a[0]
+    } else {
+        let r = sum(a[0..a.len() - 1].to_vec());
+
+        a[a.len() - 1] + r
+    }
 }
