c30035c988992e9c641ce28dd8c2e28f7774409d
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-10 18:54:05 +0900

    階差から累積和を算出

--- src/main.rs
+++ src/main.rs
@@ -4,68 +4,51 @@ use proconio::input;
 fn main() {
     input! {
         n: i64,
-        m: i64,
+        t: i64,
     }
 
-    let mut a: Vec<i64> = Vec::new();
-    let mut b: Vec<usize> = Vec::new();
-    let mut total: i64 = 0;
+    let mut l = Vec::new();
+    let mut r = Vec::new();
+    let mut a = Vec::new();
+    let mut b = Vec::new();
 
-    for i in 0..n as usize {
+    for i in 0..n + 1 {
         if i == 0 {
-            a.push(0);
+            l.push(0_i64);
+            r.push(0_i64);
         } else {
             input! {
-                ai: i64,
+                li: i64,
+                ri: i64,
             }
-            a.push(ai);
+            l.push(li);
+            r.push(ri);
         }
     }
 
-    for _ in 0..(m) as usize {
-        input! {
-            bi: usize,
-        }
-        b.push(bi);
+    for _ in 0..t + 1 {
+        a.push(0_i64);
+        b.push(0_i64);
     }
 
     let local_datetime_before: DateTime<Local> = Local::now();
 
-    let total_sum = total_sum(a);
-
-    for i in 0..b.len() {
-        if i != 0 {
-            let start = if b[i - 1] < b[i] {
-                b[i - 1] - 1
-            } else {
-                b[i] - 1
-            };
-            let end = if b[i - 1] < b[i] {
-                b[i] - 1
-            } else {
-                b[i - 1] - 1
-            };
-
-            total += total_sum[end] - total_sum[start];
-        }
+    for i in 1..(n as usize) + 1 {
+        b[l[i] as usize] += 1;
+        b[(r[i] as usize)] -= 1;
     }
 
-    println!("total move distance is {}km.", total);
+    a[0] = b[0];
+    for i in 1..(t as usize) + 1 {
+        a[i] = a[i - 1] + b[i];
+    }
+
+    for (i, _) in a.iter().enumerate().take(t as usize) {
+        println!("There are {} people at {}.5.", a[i], i);
+    }
 
     let local_datetime_after: DateTime<Local> = Local::now();
 
     let code_duration = local_datetime_after - local_datetime_before;
     println!("time: {}", code_duration.num_milliseconds());
-}
-
-fn total_sum(a: Vec<i64>) -> Vec<i64> {
-    let mut t = Vec::new();
-    for i in 0..a.len() {
-        if i == 0 {
-            t.push(a[i]);
-        } else {
-            t.push(t[i - 1] + a[i]);
-        }
-    }
-    t
 }
