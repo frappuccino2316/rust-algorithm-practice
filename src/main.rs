f157b8fdea5c793e92572c1113d0b89b060fefed
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-10 22:59:04 +0900

    10^0.3の近似値を計算

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,31 @@
 use chrono::{DateTime, Local};
-use proconio::input;
+// use proconio::input;
 
 fn main() {
-    input! {
-        n: i64,
-        t: i64,
-    }
-
-    let mut l = Vec::new();
-    let mut r = Vec::new();
-    let mut a = Vec::new();
-    let mut b = Vec::new();
-
-    for i in 0..n + 1 {
-        if i == 0 {
-            l.push(0_i64);
-            r.push(0_i64);
-        } else {
-            input! {
-                li: i64,
-                ri: i64,
-            }
-            l.push(li);
-            r.push(ri);
-        }
-    }
-
-    for _ in 0..t + 1 {
-        a.push(0_i64);
-        b.push(0_i64);
-    }
-
     let local_datetime_before: DateTime<Local> = Local::now();
 
-    for i in 1..(n as usize) + 1 {
-        b[l[i] as usize] += 1;
-        b[(r[i] as usize)] -= 1;
+    let r = 1000.0_f64;
+    // 10^0.3 = 10√1000になる
+    let mut a = 1000_f64;
+
+    for i in 0..75 {
+        // 点 (a, f(a))の座標
+        let x = a;
+        let y = a * a * a * a * a * a * a * a * a * a;
+
+        // 上記座標の接戦を出す y = session_a * x + session_b
+        let session_a = 9.0 * x * x * x * x * x * x * x * x * x;
+        let session_b = y - session_a * x;
+
+        let next_a = (r - session_b) / session_a;
+
+        println!("Step{}: a={} -> {}.", i, a, next_a);
+
+        a = next_a;
     }
 
-    a[0] = b[0];
-    for i in 1..(t as usize) + 1 {
-        a[i] = a[i - 1] + b[i];
-    }
-
-    for (i, _) in a.iter().enumerate().take(t as usize) {
-        println!("There are {} people at {}.5.", a[i], i);
-    }
+    let answer = 10.0_f64.powf(0.3);
+    println!("answer: {}", answer);
 
     let local_datetime_after: DateTime<Local> = Local::now();
 
