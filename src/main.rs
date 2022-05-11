cbaef9f5b37c7889d80636ea4511cbbe46e3d6d3
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-11 16:43:02 +0900

    幅優先探索でスタートからゴールへの最短距離を計算

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,85 @@
 use proconio::input;
+use std::collections::VecDeque;
 
 fn main() {
     input! {
-        n: usize,
-        m: usize,
+        r: usize,
+        c: usize,
+        sy: usize,
+        sx: usize,
+        gy: usize,
+        gx: usize,
     }
 
-    let mut g = vec![vec![]; n + 1];
-    let mut result = 0;
+    let mut board: Vec<Vec<char>> = vec![vec![]; r];
+    let mut g: Vec<Vec<usize>> = vec![vec![]; (r - 2) * (c - 2) + 1];
 
-    for _ in 1..=m {
+    for i in 0..r {
         input! {
-            ai: usize,
-            bi: usize,
+            cl: String,
         }
-        g[ai].push(bi);
-        g[bi].push(ai);
+
+        let row: Vec<char> = cl.chars().collect();
+        for j in row {
+            board[i].push(j);
+        }
     }
 
-    for (i, _) in g.iter().enumerate().take(n + 1).skip(1) {
-        let mut tmp_count = 0;
-        for j in &g[i] {
-            if *j < i {
-                tmp_count += 1;
+    for i in 1..r - 1 {
+        for j in 1..c {
+            if board[i][j] == '.' && board[i][j + 1] == '.' {
+                let idx1 = (i - 1) * (c - 2) + j;
+                let idx2 = (i - 1) * (c - 2) + j + 1;
+                g[idx1].push(idx2);
+                g[idx2].push(idx1);
             }
         }
-        if tmp_count == 1 {
-            result += 1;
+    }
+
+    for i in 1..r {
+        for j in 1..c {
+            if board[i][j] == '.' && board[i + 1][j] == '.' {
+                let idx1 = (i - 1) * (c - 2) + j;
+                let idx2 = i * (c - 2) + j;
+                g[idx1].push(idx2);
+                g[idx2].push(idx1);
+            }
         }
     }
 
-    println!("{}", result);
+    // スタート座標の頂点番号
+    let start_id = (sy - 2) * (c - 2) + sx - 1;
+    // ゴール座標の頂点番号
+    let goal_id = (gy - 2) * (c - 2) + gx - 1;
+
+    // ↓から幅優先探索
+    // キューを作成し、start_idを初期値に入れる
+    let mut queue = VecDeque::new();
+    queue.push_back(start_id);
+
+    // 拡張点の最短距離を格納する変数を作成
+    // スタート位置の頂点は0
+    let mut dist = vec![-1; g.len()];
+    dist[start_id] = 0;
+
+    // 隣接する頂点のうち小さい順に探索するため、隣接する頂点のリストをソートしておく
+    for adjacent_list in &mut g {
+        adjacent_list.sort_unstable();
+    }
+
+    while !queue.is_empty() {
+        // キューの先頭をチェック
+        let pos = queue[0];
+        queue.pop_front();
+
+        for i in 0..g[pos].len() {
+            let nex = g[pos][i];
+            if dist[nex] == -1 {
+                dist[nex] = dist[pos] + 1;
+                queue.push_back(nex);
+            }
+        }
+    }
+
+    println!("{}", dist[goal_id]);
 }
