497c08e8848d10bf55ec0c6537494afc92bc331c
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-11 10:26:09 +0900

    エラトステネスのふるい

--- src/main.rs
+++ src/main.rs
@@ -2,45 +2,33 @@ use proconio::input;
 
 fn main() {
     input! {
-        t: i64,
         n: i64,
     }
 
-    let mut l = Vec::new();
-    let mut r = Vec::new();
-    let mut a = Vec::new();
-    let mut b = Vec::new();
-
+    let mut prime = Vec::new();
     for i in 0..n + 1 {
-        if i == 0 {
-            l.push(0_i64);
-            r.push(0_i64);
+        if i < 2 {
+            prime.push(false)
         } else {
-            input! {
-                li: i64,
-                ri: i64,
-            }
-            l.push(li);
-            r.push(ri);
+            prime.push(true);
         }
     }
 
-    for _ in 0..t + 1 {
-        a.push(0_i64);
-        b.push(0_i64);
+    let mut j = 2_usize;
+    while j * j < n.try_into().unwrap() {
+        if prime[j] {
+            let mut x = j * 2;
+            while x <= n.try_into().unwrap() {
+                prime[x] = false;
+                x += j;
+            }
+        }
+        j += 1;
     }
 
-    for i in 1..(n as usize) + 1 {
-        b[l[i] as usize] += 1;
-        b[(r[i] as usize)] -= 1;
-    }
-
-    a[0] = b[0];
-    for i in 1..(t as usize) + 1 {
-        a[i] = a[i - 1] + b[i];
-    }
-
-    for (i, _) in a.iter().enumerate().take(t as usize) {
-        println!("{}", a[i]);
+    for (k, p) in prime.iter().enumerate() {
+        if *p {
+            println!("{}", k);
+        }
     }
 }
